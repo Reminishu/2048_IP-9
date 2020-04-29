bfd087a08ea02f8c6c4375db87f0ce418c48598d
55388899 <wraith@pop-os.localdomain>
2020-04-29 14:06:02 +0530

    Added Base UserInput Code

--- main.js
+++ main.js
@@ -18,7 +18,25 @@ function reset()
 }
 
 // Input listener
-
+function UserInput(){
+document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        //Add code to flow output where needed.
+        case 37:
+            alert('left');
+            break;
+        case 38:
+            alert('up');
+            break;
+        case 39:
+            alert('right');
+            break;
+        case 40:
+            alert('down');
+            break;
+    }
+};
+};
 // Move
 function move()
 {
