59f2e323d93f7b40eefee1f9d0447cc44c8e007c
notkanishk <singhkanishka1@gmail.com>
2020-04-30 18:17:38 +0530

    tile change color v1.0

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ function move(key)
                             cell[zeroIndex] = cell[j]
                             cell[j] = 0
                             headIndex = zeroIndex
-                            zeroIndex = j
+                            zeroIndex+=4
                         }
                     }
                     else if(cell[j] === 0 && !zeroFlag)     //first zero at start or after a unique value
@@ -124,7 +124,7 @@ function move(key)
                             cell[zeroIndex] = cell[j]
                             cell[j] = 0
                             headIndex = zeroIndex
-                            zeroIndex = j
+                            zeroIndex--
                         }
                     }
                     else if(cell[j] === 0 && !zeroFlag)     //first zero at start or after a unique value
@@ -159,7 +159,7 @@ function move(key)
                             cell[zeroIndex] = cell[j]
                             cell[j] = 0
                             headIndex = zeroIndex
-                            zeroIndex = j
+                            zeroIndex-=4
                         }
                     }
                     else if(cell[j] === 0 && !zeroFlag)     //first zero at start or after a unique value
@@ -194,7 +194,7 @@ function move(key)
                             cell[zeroIndex] = cell[j]
                             cell[j] = 0
                             headIndex = zeroIndex
-                            zeroIndex = j
+                            zeroIndex++
                         }
                     }
                     else if(cell[j] === 0 && !zeroFlag)     //first zero at start or after a unique value
@@ -290,7 +290,7 @@ function draw()
     const tile1 =document.getElementById('tile1')
     const tile2 =document.getElementById('tile2')
     const tile3 =document.getElementById('tile3')
-    const tile4 =document.getElementById('tile4')
+    const tile4 =document.getElementById('tile1')
     const tile5 =document.getElementById('tile5')
     const tile6 =document.getElementById('tile6')
     const tile7 =document.getElementById('tile7')
@@ -329,21 +329,336 @@ function draw()
         if(cell[0] === 65536){tile1.className = 'num65536'}
         if(cell[0] === 131072){tile1.className = 'num131072'}
     }
-    if(tile2){tile2.textContent=  cell[1]}
-    if(tile3){tile3.textContent=  cell[2]}
-    if(tile4){tile4.textContent=  cell[3]}
-    if(tile5){tile5.textContent=  cell[4]}
-    if(tile6){tile6.textContent=  cell[5]}
-    if(tile7){tile7.textContent=  cell[6]}
-    if(tile8){tile8.textContent=  cell[7]}
-    if(tile9){tile9.textContent=  cell[8]}
-    if(tile10){tile10.textContent= cell[9]}
-    if(tile11){tile11.textContent= cell[10]}
-    if(tile12){tile12.textContent= cell[11]}
-    if(tile13){tile13.textContent= cell[12]}
-    if(tile14){tile14.textContent= cell[13]}
-    if(tile15){tile15.textContent= cell[14]}
-    if(tile16){tile16.textContent= cell[15]}
+    if(tile2)
+    {
+        tile2.textContent=  cell[1]
+        if(cell[1] === 0){tile2.className = 'num0'}
+        if(cell[1] === 2){tile2.className = 'num2'}
+        if(cell[1] === 4){tile2.className = 'num4'}
+        if(cell[1] === 8){tile2.className = 'num8'}
+        if(cell[1] === 16){tile2.className = 'num16'}
+        if(cell[1] === 32){tile2.className = 'num32'}
+        if(cell[1] === 64){tile2.className = 'num64'}
+        if(cell[1] === 128){tile2.className = 'num128'}
+        if(cell[1] === 256){tile2.className = 'num256'}
+        if(cell[1] === 512){tile2.className = 'num512'}
+        if(cell[1] === 1024){tile2.className = 'num1024'}
+        if(cell[1] === 2048){tile2.className = 'num2048'}
+        if(cell[1] === 4096){tile2.className = 'num4096'}
+        if(cell[1] === 8192){tile2.className = 'num8192'}
+        if(cell[1] === 16384){tile2.className = 'num16384'}
+        if(cell[1] === 32768){tile2.className = 'num32768'}
+        if(cell[1] === 65536){tile2.className = 'num65536'}
+        if(cell[1] === 131072){tile2.className = 'num131072'}
+    }
+    if(tile3)
+    {
+        tile3.textContent=  cell[2]
+        if(cell[2] === 0){tile3.className = 'num0'}
+        if(cell[2] === 2){tile3.className = 'num2'}
+        if(cell[2] === 4){tile3.className = 'num4'}
+        if(cell[2] === 8){tile3.className = 'num8'}
+        if(cell[2] === 16){tile3.className = 'num16'}
+        if(cell[2] === 32){tile3.className = 'num32'}
+        if(cell[2] === 64){tile3.className = 'num64'}
+        if(cell[2] === 128){tile3.className = 'num128'}
+        if(cell[2] === 256){tile3.className = 'num256'}
+        if(cell[2] === 512){tile3.className = 'num512'}
+        if(cell[2] === 1024){tile3.className = 'num1024'}
+        if(cell[2] === 2048){tile3.className = 'num2048'}
+        if(cell[2] === 4096){tile3.className = 'num4096'}
+        if(cell[2] === 8192){tile3.className = 'num8192'}
+        if(cell[2] === 16384){tile3.className = 'num16384'}
+        if(cell[2] === 32768){tile3.className = 'num32768'}
+        if(cell[2] === 65536){tile3.className = 'num65536'}
+        if(cell[2] === 131072){tile3.className = 'num131072'}
+    }
+    if(tile4)
+    {
+        tile4.textContent=  cell[3]
+        if(cell[3] === 0){tile4.className = 'num0'}
+        if(cell[3] === 2){tile4.className = 'num2'}
+        if(cell[3] === 4){tile4.className = 'num4'}
+        if(cell[3] === 8){tile4.className = 'num8'}
+        if(cell[3] === 16){tile4.className = 'num16'}
+        if(cell[3] === 32){tile4.className = 'num32'}
+        if(cell[3] === 64){tile4.className = 'num64'}
+        if(cell[3] === 128){tile4.className = 'num128'}
+        if(cell[3] === 256){tile4.className = 'num256'}
+        if(cell[3] === 512){tile4.className = 'num512'}
+        if(cell[3] === 1024){tile4.className = 'num1024'}
+        if(cell[3] === 2048){tile4.className = 'num2048'}
+        if(cell[3] === 4096){tile4.className = 'num4096'}
+        if(cell[3] === 8192){tile4.className = 'num8192'}
+        if(cell[3] === 16384){tile4.className = 'num16384'}
+        if(cell[3] === 32768){tile4.className = 'num32768'}
+        if(cell[3] === 65536){tile4.className = 'num65536'}
+        if(cell[3] === 131072){tile4.className = 'num131072'}
+    }
+    if(tile5)
+    {
+        tile5.textContent=  cell[4]
+        if(cell[4] === 0){tile5.className = 'num0'}
+        if(cell[4] === 2){tile5.className = 'num2'}
+        if(cell[4] === 4){tile5.className = 'num4'}
+        if(cell[4] === 8){tile5.className = 'num8'}
+        if(cell[4] === 16){tile5.className = 'num16'}
+        if(cell[4] === 32){tile5.className = 'num32'}
+        if(cell[4] === 64){tile5.className = 'num64'}
+        if(cell[4] === 128){tile5.className = 'num128'}
+        if(cell[4] === 256){tile5.className = 'num256'}
+        if(cell[4] === 512){tile5.className = 'num512'}
+        if(cell[4] === 1024){tile5.className = 'num1024'}
+        if(cell[4] === 2048){tile5.className = 'num2048'}
+        if(cell[4] === 4096){tile5.className = 'num4096'}
+        if(cell[4] === 8192){tile5.className = 'num8192'}
+        if(cell[4] === 16384){tile5.className = 'num16384'}
+        if(cell[4] === 32768){tile5.className = 'num32768'}
+        if(cell[4] === 65536){tile5.className = 'num65536'}
+        if(cell[4] === 131072){tile5.className = 'num131072'}
+    }
+    if(tile6)
+    {
+        tile6.textContent=  cell[5]
+        if(cell[5] === 0){tile6.className = 'num0'}
+        if(cell[5] === 2){tile6.className = 'num2'}
+        if(cell[5] === 4){tile6.className = 'num4'}
+        if(cell[5] === 8){tile6.className = 'num8'}
+        if(cell[5] === 16){tile6.className = 'num16'}
+        if(cell[5] === 32){tile6.className = 'num32'}
+        if(cell[5] === 64){tile6.className = 'num64'}
+        if(cell[5] === 128){tile6.className = 'num128'}
+        if(cell[5] === 256){tile6.className = 'num256'}
+        if(cell[5] === 512){tile6.className = 'num512'}
+        if(cell[5] === 1024){tile6.className = 'num1024'}
+        if(cell[5] === 2048){tile6.className = 'num2048'}
+        if(cell[5] === 4096){tile6.className = 'num4096'}
+        if(cell[5] === 8192){tile6.className = 'num8192'}
+        if(cell[5] === 16384){tile6.className = 'num16384'}
+        if(cell[5] === 32768){tile6.className = 'num32768'}
+        if(cell[5] === 65536){tile6.className = 'num65536'}
+        if(cell[5] === 131072){tile6.className = 'num131072'}
+    }
+    if(tile7)
+    {
+        tile7.textContent=  cell[6]
+        if(cell[6] === 0){tile7.className = 'num0'}
+        if(cell[6] === 2){tile7.className = 'num2'}
+        if(cell[6] === 4){tile7.className = 'num4'}
+        if(cell[6] === 8){tile7.className = 'num8'}
+        if(cell[6] === 16){tile7.className = 'num16'}
+        if(cell[6] === 32){tile7.className = 'num32'}
+        if(cell[6] === 64){tile7.className = 'num64'}
+        if(cell[6] === 128){tile7.className = 'num128'}
+        if(cell[6] === 256){tile7.className = 'num256'}
+        if(cell[6] === 512){tile7.className = 'num512'}
+        if(cell[6] === 1024){tile7.className = 'num1024'}
+        if(cell[6] === 2048){tile7.className = 'num2048'}
+        if(cell[6] === 4096){tile7.className = 'num4096'}
+        if(cell[6] === 8192){tile7.className = 'num8192'}
+        if(cell[6] === 16384){tile7.className = 'num16384'}
+        if(cell[6] === 32768){tile7.className = 'num32768'}
+        if(cell[6] === 65536){tile7.className = 'num65536'}
+        if(cell[6] === 131072){tile7.className = 'num131072'}
+    }
+    if(tile8)
+    {
+        tile8.textContent=  cell[7]
+        if(cell[7] === 0){tile8.className = 'num0'}
+        if(cell[7] === 2){tile8.className = 'num2'}
+        if(cell[7] === 4){tile8.className = 'num4'}
+        if(cell[7] === 8){tile8.className = 'num8'}
+        if(cell[7] === 16){tile8.className = 'num16'}
+        if(cell[7] === 32){tile8.className = 'num32'}
+        if(cell[7] === 64){tile8.className = 'num64'}
+        if(cell[7] === 128){tile8.className = 'num128'}
+        if(cell[7] === 256){tile8.className = 'num256'}
+        if(cell[7] === 512){tile8.className = 'num512'}
+        if(cell[7] === 1024){tile8.className = 'num1024'}
+        if(cell[7] === 2048){tile8.className = 'num2048'}
+        if(cell[7] === 4096){tile8.className = 'num4096'}
+        if(cell[7] === 8192){tile8.className = 'num8192'}
+        if(cell[7] === 16384){tile8.className = 'num16384'}
+        if(cell[7] === 32768){tile8.className = 'num32768'}
+        if(cell[7] === 65536){tile8.className = 'num65536'}
+        if(cell[7] === 131072){tile8.className = 'num131072'}
+    }
+    if(tile9)
+    {
+        tile9.textContent=  cell[8]
+        if(cell[8] === 0){tile9.className = 'num0'}
+        if(cell[8] === 2){tile9.className = 'num2'}
+        if(cell[8] === 4){tile9.className = 'num4'}
+        if(cell[8] === 8){tile9.className = 'num8'}
+        if(cell[8] === 16){tile9.className = 'num16'}
+        if(cell[8] === 32){tile9.className = 'num32'}
+        if(cell[8] === 64){tile9.className = 'num64'}
+        if(cell[8] === 128){tile9.className = 'num128'}
+        if(cell[8] === 256){tile9.className = 'num256'}
+        if(cell[8] === 512){tile9.className = 'num512'}
+        if(cell[8] === 1024){tile9.className = 'num1024'}
+        if(cell[8] === 2048){tile9.className = 'num2048'}
+        if(cell[8] === 4096){tile9.className = 'num4096'}
+        if(cell[8] === 8192){tile9.className = 'num8192'}
+        if(cell[8] === 16384){tile9.className = 'num16384'}
+        if(cell[8] === 32768){tile9.className = 'num32768'}
+        if(cell[8] === 65536){tile9.className = 'num65536'}
+        if(cell[8] === 131072){tile9.className = 'num131072'}
+    }
+    if(tile10)
+    {
+        tile10.textContent= cell[9]
+        if(cell[9] === 0){tile10.className = 'num0'}
+        if(cell[9] === 2){tile10.className = 'num2'}
+        if(cell[9] === 4){tile10.className = 'num4'}
+        if(cell[9] === 8){tile10.className = 'num8'}
+        if(cell[9] === 16){tile10.className = 'num16'}
+        if(cell[9] === 32){tile10.className = 'num32'}
+        if(cell[9] === 64){tile10.className = 'num64'}
+        if(cell[9] === 128){tile10.className = 'num128'}
+        if(cell[9] === 256){tile10.className = 'num256'}
+        if(cell[9] === 512){tile10.className = 'num512'}
+        if(cell[9] === 1024){tile10.className = 'num1024'}
+        if(cell[9] === 2048){tile10.className = 'num2048'}
+        if(cell[9] === 4096){tile10.className = 'num4096'}
+        if(cell[9] === 8192){tile10.className = 'num8192'}
+        if(cell[9] === 16384){tile10.className = 'num16384'}
+        if(cell[9] === 32768){tile10.className = 'num32768'}
+        if(cell[9] === 65536){tile10.className = 'num65536'}
+        if(cell[9] === 131072){tile10.className = 'num131072'}
+    }
+    if(tile11)
+    {
+        tile11.textContent= cell[10]
+        if(cell[10] === 0){tile11.className = 'num0'}
+        if(cell[10] === 2){tile11.className = 'num2'}
+        if(cell[10] === 4){tile11.className = 'num4'}
+        if(cell[10] === 8){tile11.className = 'num8'}
+        if(cell[10] === 16){tile11.className = 'num16'}
+        if(cell[10] === 32){tile11.className = 'num32'}
+        if(cell[10] === 64){tile11.className = 'num64'}
+        if(cell[10] === 128){tile11.className = 'num128'}
+        if(cell[10] === 256){tile11.className = 'num256'}
+        if(cell[10] === 512){tile11.className = 'num512'}
+        if(cell[10] === 1024){tile11.className = 'num1024'}
+        if(cell[10] === 2048){tile11.className = 'num2048'}
+        if(cell[10] === 4096){tile11.className = 'num4096'}
+        if(cell[10] === 8192){tile11.className = 'num8192'}
+        if(cell[10] === 16384){tile11.className = 'num16384'}
+        if(cell[10] === 32768){tile11.className = 'num32768'}
+        if(cell[10] === 65536){tile11.className = 'num65536'}
+        if(cell[10] === 131072){tile11.className = 'num131072'}
+    }
+    if(tile12)
+    {
+        tile12.textContent= cell[11]
+        if(cell[11] === 0){tile12.className = 'num0'}
+        if(cell[11] === 2){tile12.className = 'num2'}
+        if(cell[11] === 4){tile12.className = 'num4'}
+        if(cell[11] === 8){tile12.className = 'num8'}
+        if(cell[11] === 16){tile12.className = 'num16'}
+        if(cell[11] === 32){tile12.className = 'num32'}
+        if(cell[11] === 64){tile12.className = 'num64'}
+        if(cell[11] === 128){tile12.className = 'num128'}
+        if(cell[11] === 256){tile12.className = 'num256'}
+        if(cell[11] === 512){tile12.className = 'num512'}
+        if(cell[11] === 1024){tile12.className = 'num1024'}
+        if(cell[11] === 2048){tile12.className = 'num2048'}
+        if(cell[11] === 4096){tile12.className = 'num4096'}
+        if(cell[11] === 8192){tile12.className = 'num8192'}
+        if(cell[11] === 16384){tile12.className = 'num16384'}
+        if(cell[11] === 32768){tile12.className = 'num32768'}
+        if(cell[11] === 65536){tile12.className = 'num65536'}
+        if(cell[11] === 131072){tile12.className = 'num131072'}
+    }
+    if(tile13)
+    {
+        tile13.textContent= cell[12]
+        if(cell[12] === 0){tile13.className = 'num0'}
+        if(cell[12] === 2){tile13.className = 'num2'}
+        if(cell[12] === 4){tile13.className = 'num4'}
+        if(cell[12] === 8){tile13.className = 'num8'}
+        if(cell[12] === 16){tile13.className = 'num16'}
+        if(cell[12] === 32){tile13.className = 'num32'}
+        if(cell[12] === 64){tile13.className = 'num64'}
+        if(cell[12] === 128){tile13.className = 'num128'}
+        if(cell[12] === 256){tile13.className = 'num256'}
+        if(cell[12] === 512){tile13.className = 'num512'}
+        if(cell[12] === 1024){tile13.className = 'num1024'}
+        if(cell[12] === 2048){tile13.className = 'num2048'}
+        if(cell[12] === 4096){tile13.className = 'num4096'}
+        if(cell[12] === 8192){tile13.className = 'num8192'}
+        if(cell[12] === 16384){tile13.className = 'num16384'}
+        if(cell[12] === 32768){tile13.className = 'num32768'}
+        if(cell[12] === 65536){tile13.className = 'num65536'}
+        if(cell[12] === 131072){tile13.className = 'num131072'}
+    }
+    if(tile14)
+    {
+        tile14.textContent= cell[13]
+        if(cell[13] === 0){tile14.className = 'num0'}
+        if(cell[13] === 2){tile14.className = 'num2'}
+        if(cell[13] === 4){tile14.className = 'num4'}
+        if(cell[13] === 8){tile14.className = 'num8'}
+        if(cell[13] === 16){tile14.className = 'num16'}
+        if(cell[13] === 32){tile14.className = 'num32'}
+        if(cell[13] === 64){tile14.className = 'num64'}
+        if(cell[13] === 128){tile14.className = 'num128'}
+        if(cell[13] === 256){tile14.className = 'num256'}
+        if(cell[13] === 512){tile14.className = 'num512'}
+        if(cell[13] === 1024){tile14.className = 'num1024'}
+        if(cell[13] === 2048){tile14.className = 'num2048'}
+        if(cell[13] === 4096){tile14.className = 'num4096'}
+        if(cell[13] === 8192){tile14.className = 'num8192'}
+        if(cell[13] === 16384){tile14.className = 'num16384'}
+        if(cell[13] === 32768){tile14.className = 'num32768'}
+        if(cell[13] === 65536){tile14.className = 'num65536'}
+        if(cell[13] === 131072){tile14.className = 'num131072'}
+    }
+    if(tile15)
+    {
+        tile15.textContent= cell[14]
+        if(cell[14] === 0){tile15.className = 'num0'}
+        if(cell[14] === 2){tile15.className = 'num2'}
+        if(cell[14] === 4){tile15.className = 'num4'}
+        if(cell[14] === 8){tile15.className = 'num8'}
+        if(cell[14] === 16){tile15.className = 'num16'}
+        if(cell[14] === 32){tile15.className = 'num32'}
+        if(cell[14] === 64){tile15.className = 'num64'}
+        if(cell[14] === 128){tile15.className = 'num128'}
+        if(cell[14] === 256){tile15.className = 'num256'}
+        if(cell[14] === 512){tile15.className = 'num512'}
+        if(cell[14] === 1024){tile15.className = 'num1024'}
+        if(cell[14] === 2048){tile15.className = 'num2048'}
+        if(cell[14] === 4096){tile15.className = 'num4096'}
+        if(cell[14] === 8192){tile15.className = 'num8192'}
+        if(cell[14] === 16384){tile15.className = 'num16384'}
+        if(cell[14] === 32768){tile15.className = 'num32768'}
+        if(cell[14] === 65536){tile15.className = 'num65536'}
+        if(cell[14] === 131072){tile15.className = 'num131072'}
+    }
+    if(tile16)
+    {
+        tile16.textContent= cell[15]
+        if(cell[15] === 0){tile16.className = 'num0'}
+        if(cell[15] === 2){tile16.className = 'num2'}
+        if(cell[15] === 4){tile16.className = 'num4'}
+        if(cell[15] === 8){tile16.className = 'num8'}
+        if(cell[15] === 16){tile16.className = 'num16'}
+        if(cell[15] === 32){tile16.className = 'num32'}
+        if(cell[15] === 64){tile16.className = 'num64'}
+        if(cell[15] === 128){tile16.className = 'num128'}
+        if(cell[15] === 256){tile16.className = 'num256'}
+        if(cell[15] === 512){tile16.className = 'num512'}
+        if(cell[15] === 1024){tile16.className = 'num1024'}
+        if(cell[15] === 2048){tile16.className = 'num2048'}
+        if(cell[15] === 4096){tile16.className = 'num4096'}
+        if(cell[15] === 8192){tile16.className = 'num8192'}
+        if(cell[15] === 16384){tile16.className = 'num16384'}
+        if(cell[15] === 32768){tile16.className = 'num32768'}
+        if(cell[15] === 65536){tile16.className = 'num65536'}
+        if(cell[15] === 131072){tile16.className = 'num131072'}
+    }
 }
 
 
